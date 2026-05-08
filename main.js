c53ad47550fce3efa7f84ba0b4abfcc71ca6c9dc
K-N9N3-X <k.n.abregu@hotmail.com>
2026-05-08 02:48:22 -0400

    create css and js files. Random number for correct console return

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// get a number between 1 and 3 //
+
+function getComputerChoice() {
+    function getRandomInt(max) {
+         return Math.floor(Math.random() * max);
+    } 
+   console.log(getRandomInt(3));
+}
+getComputerChoice();
+
+// convert numbers to rock, paper, or scissors //
+
